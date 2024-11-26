af17f54ee10c9eb01090d2eaa8b226f58b785f66
SameerKhan <sameerkhanyt09@gmail.com>
2024-11-26 13:55:38 +0530

    constants

--- src/main/java/com/sam/blog/controllers/PostController.java
+++ src/main/java/com/sam/blog/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.sam.blog.controllers;
 
+import com.sam.blog.config.AppConstants;
 import com.sam.blog.payloads.ApiResponse;
 import com.sam.blog.payloads.PostDTO;
 import com.sam.blog.payloads.PostResponse;
@@ -40,10 +41,10 @@ public class PostController {
 
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-			@RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize,
-			@RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-			@RequestParam(value = "sortDir", defaultValue = "ASC", required = false) String sortDir
+			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE required= false) Integer pageSize,
+			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir
 	) {
 		PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
 		return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
